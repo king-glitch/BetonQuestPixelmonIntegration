9afc83caa62b8f403350ca678b6d04c447620892
king-glitch <wilhelm.hsf@gmail.com>
2023-10-21 00:28:06 +0700

    feat(structure): first project structure

--- modules/v1_16_5/src/main/java/dev/rachamon/betonquestpixelmonintegration/v1_16_5/reforged/Main.java
+++
@@ -1,7 +0,0 @@
-package dev.rachamon.betonquestpixelmonintegration.v1_16_5.reforged;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- src/main/java/dev/rachamon/betonquestpixelmonintegration/BetonQuestPixelmonIntegration.java
+++ src/main/java/dev/rachamon/betonquestpixelmonintegration/BetonQuestPixelmonIntegration.java
@@ -1,6 +1,40 @@
 package dev.rachamon.betonquestpixelmonintegration;
+
+import dev.rachamon.betonquestpixelmonintegration.commons.factory.IntegrationEventFactory;
+import lombok.Getter;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class BetonQuestPixelmonIntegration extends JavaPlugin{
+public class BetonQuestPixelmonIntegration extends JavaPlugin {
+    @Getter
+    public static BetonQuestPixelmonIntegration instance;
+    @Getter
+    private IntegrationEventFactory integrationEventFactory;
 
+    public BetonQuestPixelmonIntegration() {
+        instance = this;
+    }
+
+    @Override
+    public void onEnable() {
+        System.out.println("Starting plugin...");
+
+        if (Bukkit.getPluginManager().getPlugin("BetonQuest") == null) {
+            System.out.println("BetonQuest not found!");
+            Bukkit.getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        System.out.println("injecting events...");
+        integrationEventFactory = IntegrationEventFactory.create();
+        System.out.println("events injected!");
+
+        System.out.println("Plugin Loaded!");
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("Stopping plugin...");
+        integrationEventFactory.unregister();
+    }
 }
